75a06f6d16d2d27244822bf5336bd130ceb0eb28
G P <gp@localhost.localdomain>
2018-02-12 23:32:42 +0100

    Get rid of the useless manageQuotes function.

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,48 @@
+"use strict";
 window.onload = function () {
-    //animate when a new quote is requested
+    // Animate when a new quote is requested
     var main = document.getElementById("main");
-    var quoteButton = document.getElementById("quoteButton"); 
+    var quoteButton = document.getElementById("quoteButton");
+    // When new quote button is clicked
     quoteButton.addEventListener("click", function(){
+	// Make quote fade out right
 	main.setAttribute("class", "animated fadeOutRightBig");
+	// And make quote fade in left after 500 ms (after getQuotes has already been called)
 	setTimeout(function(){
 	    main.setAttribute("class", "animated fadeInLeftBig");
-	}, 500)
+	}, 500);
     });
 
     getQuotes();
-}
-
-// get and array of 40 quotes and display one
-var displayRandomQuote = function (quotes) {
-    //generate random number between 0 and 39 
-    var quoteIndex =  Math.floor(Math.random() * 40); 
-    //get a random quote object from the array
-    var randomQuote = quotes[quoteIndex];
-    //put the quote text into #quote (eliminating loading circle)
-    var quoteElementNode = document.getElementById("quote");
-    quoteElementNode.innerHTML = randomQuote.content;
-    //put the quote author into #author 
-    var authorElementNode = document.getElementById("author");
-    authorElementNode.innerHTML = randomQuote.title;
 };
 
-var manageQuotes = function(quotes) {
-    displayRandomQuote(quotes); 
-
-    setTwitterButton();
-}
-
-//create script element to get quotes (JSONP)
+// Create, append (and remove) script element to get quotes (JSONP)
 var getQuotes = function() {
     var scriptQuote = document.createElement("script");
     scriptQuote.id = "getQuotes";
-    scriptQuote.src = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&_jsonp=manageQuotes";
+    scriptQuote.src = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&_jsonp=displayRandomQuote";
     document.body.appendChild(scriptQuote);
-    //remove script element
+    // Remove script element
     document.getElementById(scriptQuote.id).remove();
 };
 
-//put right link in twitter button
+// Get and array of 40 quotes and display one
+var displayRandomQuote = function (quotes) {
+    // Generate random number between 0 and 39 
+    var quoteIndex =  Math.floor(Math.random() * 40); 
+    // Get a random quote object from the array
+    var randomQuote = quotes[quoteIndex];
+    // Put the quote text into #quote (replacing loading circle)
+    var quoteElementNode = document.getElementById("quote");
+    quoteElementNode.innerHTML = randomQuote.content;
+    // Put the quote author into #author 
+    var authorElementNode = document.getElementById("author");
+    authorElementNode.innerHTML = randomQuote.title;
+
+    setTwitterButton();
+};
+
+// Put right link in twitter button
 var setTwitterButton = function () {
     var text = document.getElementById("quote").firstChild.firstChild.nodeValue;
 
